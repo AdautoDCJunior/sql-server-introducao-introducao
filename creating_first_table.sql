8fe9493ee5a3d612a6cc72e6ba4f9b1c124c5b5c
Adauto Júnior <adautodcjunior@gmail.com>
2022-07-26 20:24:11 -0300

    Finishing class 02

---
+++ creating_first_table.sql
@@ -0,0 +1,16 @@
+create table [client_table] (
+    [cpf] [VARCHAR](11),
+    [name] [VARCHAR](100),
+    [address_1] [VARCHAR](150),
+    [address_2] [VARCHAR](150),
+    [district] [VARCHAR](50),
+    [city] [VARCHAR](50),
+    [state] [VARCHAR](2),
+    [cep] [VARCHAR](8),
+    [birthday] [DATE],
+    [age] [SMALLINT],
+    [sex] [VARCHAR](1),
+    [credit_limit] [MONEY],
+    [purchase_volume] [FLOAT],
+    [first_purchase] [BIT]
+);
